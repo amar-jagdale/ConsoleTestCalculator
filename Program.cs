c0572fb3a06499231b3496684d9ea03342a433e8
hp <hp@DESKTOP-TL0FRLL>
2021-09-01 14:20:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibraryDemoCalculator;
+namespace ConsoleTestCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+            Class1 obj1 = new Class1();
+            obj1.add();
+            obj1.sub();
+            obj1.mul();
+           
+        }
+    }
+}
