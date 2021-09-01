e5c6f0b165346a6306977ab5e42dcc4565d9b570
hp <hp@DESKTOP-TL0FRLL>
2021-09-01 14:21:15 +0530

    Program to implement calculator

--- Program.cs
+++ Program.cs
@@ -1,7 +1,32 @@
 ﻿using System;
-using ClassLibraryDemoCalculator;
+
 namespace ConsoleTestCalculator
 {
+    public class Class1
+    {
+        double num1 = 40;
+        double num2 = 30;
+        double res;
+        public void add()
+        {
+            res = num1 + num2;
+            Console.WriteLine("Addition of Two num is: " + res);
+        }
+
+        public void sub()
+        {
+
+            res = num1 - num2;
+            Console.WriteLine("Substraction of Two num is: " + res);
+        }
+
+        public void mul()
+        {
+            res = num1 * num2;
+            Console.WriteLine("Multiplication of Two num is: " + res);
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
